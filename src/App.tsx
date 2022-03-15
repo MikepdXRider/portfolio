301a4812b17c9343cddb820e301af9ca21afeae4
Michael Rider <mikepdxrider@gmail.com>
2022-03-14 22:10:17 -0700

    renders basic hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
 import Section from './components/Section/Section';
+import portrait from './assets/round-portrait.png';
 
 function App() {
   return (
     <main className="App">
       <Section darkMode={true}>
-        <p>Hello Hero Section!</p>
+        {/* return container*/}
+        <div>
+          {/* text container */}
+          <div>
+            <h1>Michael X Rider</h1>
+            <h3>Software Engineer</h3>
+          </div>
+          <img src={portrait} alt="Profile" />
+        </div>
       </Section>
       <Section title={'About Me'}>
         <p>Hello About Me Section!</p>
