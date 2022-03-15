00afd8ada9c982dcb5fab8999e073b6cc485fdb9
Michael Rider <mikepdxrider@gmail.com>
2022-03-14 18:32:19 -0700

    adds initial section styling

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import styles from './section.module.css';
 
 interface SectionProps {
   children: React.ReactChild;
   title?: string;
+  darkMode?: boolean;
 }
 
-export default function Section({ children, title }: SectionProps) {
+export default function Section({
+  children,
+  title,
+  darkMode = false,
+}: SectionProps) {
   return (
-    <section>
-      {title && <title>{title}</title>}
+    <section
+      className={`${darkMode ? styles.darkSection : styles.lightSection} ${
+        styles.section
+      }`}
+    >
+      {title && <h3>{title}</h3>}
 
       {children}
     </section>
