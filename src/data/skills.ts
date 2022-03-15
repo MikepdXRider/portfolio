37859a026efa6dd96d64ba35c6dc33fe095dddb7
Michael Rider <mikepdxrider@gmail.com>
2022-03-14 17:01:10 -0700

    adds skills data

---
+++ src/data/skills.ts
@@ -0,0 +1,53 @@
+export const skills = [
+  {
+    title: 'Languages',
+    content: ['JavaScript', 'HTML', 'CSS', 'SQL'],
+  },
+  {
+    title: 'Libraries',
+    content: [
+      'React',
+      'Node',
+      'Express',
+      'PostgreSQL',
+      'Superagent',
+      'Webpack',
+      'TypeScript',
+      'Tailwind',
+      'Framer Motion',
+      'oAuth',
+    ],
+  },
+  {
+    title: 'Testing Suites',
+    content: [
+      'Jest',
+      'React Testing Library',
+      'QUnit',
+      'Supertest',
+      'Mock Service Worker',
+    ],
+  },
+  {
+    title: 'Skills',
+    content: [
+      'Test Driven Development',
+      'Remote Collaboration',
+      'Pair Programming',
+      'Code Review',
+    ],
+  },
+  {
+    title: 'Dev Tools',
+    content: [
+      'VSCode',
+      'Github',
+      'Postman',
+      'Heroku',
+      'Netlify',
+      'Miro',
+      'Bash CLI',
+      'Slack',
+    ],
+  },
+];
