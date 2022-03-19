0f49186959ccef8984cc87a804cb6e68a74aaa42
Michael Rider <mikepdxrider@gmail.com>
2022-03-19 08:09:45 -0700

    adds render test

--- src/components/Contact/Contact.test.tsx
+++ src/components/Contact/Contact.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Contact from './Contact';
 
 describe('contact component', () => {
@@ -6,12 +6,26 @@ describe('contact component', () => {
     imgSrc: 'https://placekitten.com',
     imgAlt: 'mock alt',
     linkHref: 'https://placecage.com',
-    linkAriaLabel: 'mock link aria-label ',
+    linkAriaLabel: 'mock link aria-label',
   };
 
   it('matches the existing snapshot', () => {
     const { container } = render(<Contact {...mockContactProps} />);
 
     expect(container).toMatchSnapshot();
+  });
+
+  it('renders component with all passed information', () => {
+    render(<Contact {...mockContactProps} />);
+
+    // grabs elements from rendered dom.
+    const imgEl = screen.getByRole('img', { name: /mock alt/i });
+    const anchorEl = screen.getByLabelText(/mock link aria-label/i);
+
+    //checks that elements are in dom and rendered with correct info.
+    expect(imgEl).toBeInTheDocument();
+    expect(imgEl).toHaveAttribute('src', 'https://placekitten.com');
+    expect(anchorEl).toBeInTheDocument();
+    expect(anchorEl).toHaveAttribute('href', 'https://placecage.com');
   });
 });
