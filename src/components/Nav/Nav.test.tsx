a73f40f78c2732fc6e134d5137d4855362220692
Michael Rider <mikepdxrider@gmail.com>
2022-03-21 23:17:51 -0700

    adds nav tests

--- src/components/Nav/Nav.test.tsx
+++ src/components/Nav/Nav.test.tsx
@@ -1,5 +1,5 @@
 // testing
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 // components
 import Nav from './Nav';
 
@@ -21,5 +21,21 @@ describe('Nav component', () => {
     const { container } = render(<Nav navData={mockNavData} />);
 
     expect(container).toMatchSnapshot();
+  });
+
+  it('renders component with all passed information', () => {
+    render(<Nav navData={mockNavData} />);
+
+    // grabs elements from rendered dom.
+    const linkEl1 = screen.getByLabelText(/mock aria label 1/i);
+    const linkEl2 = screen.getByLabelText(/mock aria label 2/i);
+
+    //checks that elements are in dom and rendered with correct info.
+    expect(linkEl1).toBeInTheDocument();
+    expect(linkEl1).toHaveAttribute('href', 'mock href 1');
+    expect(linkEl1.textContent).toEqual('mock text content 1');
+    expect(linkEl2).toBeInTheDocument();
+    expect(linkEl2).toHaveAttribute('href', 'mock href 2');
+    expect(linkEl2.textContent).toEqual('mock text content 2');
   });
 });
