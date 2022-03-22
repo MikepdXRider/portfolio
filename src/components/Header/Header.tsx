77e8297c906378a5703313d04a402b3cba3fe1ea
Michael Rider <mikepdxrider@gmail.com>
2022-03-21 21:45:26 -0700

    adds aria-labels to header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,11 +4,21 @@ export default function Header() {
   return (
     <header>
       <nav>
-        <a href="# ">Home</a>
-        <a href="#About Me">About</a>
-        <a href="#Skills">Skills</a>
-        <a href="#Projects">Projects</a>
-        <a href="#Connect">Connect</a>
+        <a href="# " aria-label="Scrolls user to top of page">
+          Home
+        </a>
+        <a href="#About Me" aria-label="Scrolls user to About Me section">
+          About
+        </a>
+        <a href="#Skills" aria-label="Scrolls user to Sills section">
+          Skills
+        </a>
+        <a href="#Projects" aria-label="Scrolls user to Projects section">
+          Projects
+        </a>
+        <a href="#Connect" aria-label="Scrolls user to Connect section">
+          Connect
+        </a>
       </nav>
     </header>
   );
